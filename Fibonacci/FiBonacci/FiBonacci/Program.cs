58ecd934a367ec483dd50961ca9e2ad9fa4a1dea
Giorgi1971 <giooorgi7@gmail.com>
2022-10-24 23:40:54 +0400

    Fibonacci number calculation. without function and recursion

--- Fibonacci/FiBonacci/FiBonacci/Program.cs
+++ Fibonacci/FiBonacci/FiBonacci/Program.cs
@@ -1,45 +1,40 @@
 ﻿using System;
 
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            int n = 0;
-            while (true)
-            {
-                Console.WriteLine("Enter ineger: ");
-                n = Convert.ToInt32(Console.ReadLine());
-            }
-            int result = CalculateFibonacci(n);
-            Console.WriteLine(result);
-        }
 
-        long CalculateFibonacci(int n)
-        {
-            if(n == 1)
-            {
-                return 0;
-            }
-            else if (n == 2)
-            {
-                return 1;
-            }
-            else
-            {
-                int a = 0;
-                int b = 1;
-                int c = 0;
-                for (int i = 0; i < n; i++) 
-                {
-                    c = a + b;
-                    a = b;
-                    b = c;
-                }
-                return c;
-            }
-        }
+Console.WriteLine("Hello World!");
+int n = 0;
+while (true)
+{
+    Console.Write("Enter number for calculate Fibonachi number: ");
+    string inputString = Console.ReadLine();
+    if (Int32.TryParse(inputString, out n))
+    {
+        break;
     }
+    else
+    {
+        Console.WriteLine($"{inputString} is not a number\n");
+    }
+}
+
+if (n == 1)
+{
+    Console.Write(0);
+}
+else if (n == 2)
+{
+    Console.Write(1);
+}
+else
+{
+    int a = 0;
+    int b = 1;
+    int c = 0;
+    for (int i = 2; i < n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    Console.Write(c);
 }
