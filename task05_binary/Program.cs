70c762533328fa3df8f3cb72855f102939858185
Giorgi1971 <giooorgi7@gmail.com>
2022-10-27 01:18:51 +0400

    tast 5 binary from google

---
+++ task05_binary/Program.cs
@@ -0,0 +1,9 @@
+﻿string ToBinary(long number)
+{
+    return Convert.ToString(number, 2);
+}
+
+Console.WriteLine(ToBinary(2)); // 10
+Console.WriteLine(ToBinary(256)); // 100000000
+Console.WriteLine(ToBinary(33)); // 100001
+Console.WriteLine(ToBinary(999)); // 1111100111
