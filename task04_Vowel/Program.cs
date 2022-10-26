797bca7bd36c98070a5baa0597ca555988604f98
Giorgi1971 <giooorgi7@gmail.com>
2022-10-27 01:24:22 +0400

    task 4 Vowels

---
+++ task04_Vowel/Program.cs
@@ -0,0 +1,12 @@
+﻿string vowels = "aeiuoაეიოუ";
+
+bool IsVowel(char c)
+{
+    return vowels.Contains(c);
+}
+
+
+var isVowel = IsVowel('a'); // true
+isVowel = IsVowel('b'); // false
+isVowel = IsVowel('ე'); // true
+isVowel = IsVowel('კ'); // false
