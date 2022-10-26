7eedd7c161da5b5ee07807aba84f3447b55eb43d
Giorgi1971 <giooorgi7@gmail.com>
2022-10-27 01:39:22 +0400

    task 3 indexOf for search elemet in array

---
+++ task03_BinarySearch/Program.cs
@@ -0,0 +1,17 @@
+﻿int BinarySearch(int[] array, int target)
+{
+    return Array.IndexOf(array, target);
+}
+
+
+var array = new int[] { 0, 1, 5, 76, 234, 678 };
+
+var target = 76;
+
+var index = BinarySearch(array, target);
+
+Console.WriteLine(index); // index უნდა იყოს 3
+
+index = BinarySearch(array, 75);
+
+Console.WriteLine(index); // index უნდა იყოს -1
