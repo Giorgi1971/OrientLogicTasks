fa7b0af3d47215212781c16fc19c32a6df6811d8
Giorgi1971 <giooorgi7@gmail.com>
2022-11-02 15:31:03 +0400

    Made, only 2 small problems left. Writed in program comments

--- task06_Tik-tak-toe/Program.cs
+++ task06_Tik-tak-toe/Program.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 
-Console.WriteLine("Enter Player2 name: ");
-string player1 = Console.ReadLine();
-
-Console.WriteLine("Enter Player1 name: ");
-string player2 = Console.ReadLine();
-
-string currentPlayer = player1;
-
 string[,] cells;
 cells = new string[3, 3] { { "-", "-", "-" }, { "-", "-", "-" }, { "-", "-", "-" } };
 
-string mark = "X";
+Console.Write("Enter Player1 name: ");
+// არ მუშაობს ??, დამჭირდა if გამოყენება. რა უნდა?
+string player1 = Console.ReadLine() ?? "Player 1";
+if (player1 == "")
+    player1 = "Player 1";
+Console.WriteLine($"Welcome: {player1}");
 
+Console.Write("Enter Player2 name: ");
+string player2 = Console.ReadLine() ?? "Player 2";
+if (player2 == "")
+    player2 = "Player2";
+Console.WriteLine($"Welcome {player2}");
+
+string currentPlayer = player2;
+
+
+string mark = "O";
+
+drawBoard(cells);
 // არ მუშაობს, იქნებ გავარკვიო.
 bool move = (cells[0, 0] == cells[0, 1] && cells[0, 0] != "-" && cells[0, 1] == cells[0, 2]);
 //Console.WriteLine(move);
@@ -31,7 +40,7 @@ while (true)
         (cells[0, 0] == cells[1, 1] && cells[0, 0] != "-" && cells[1, 1] == cells[2, 2])
         )
     {
-        Console.WriteLine($"Congratulation You Winner!");
+        Console.WriteLine($"\nCongratulations, {currentPlayer.ToUpper()} won!");
         break;
     }
     
@@ -45,58 +54,53 @@ while (true)
         currentPlayer = player2;
         mark = "O";
     }
-    Console.Write("\n");
+    Console.WriteLine();
 
-    counter++;
-    Console.WriteLine(counter);
-    if (counter == 10)
+    if (counter == 9)
     {
-        Console.WriteLine("Congratulation Play Draw!");
-
+        Console.WriteLine("It's a Draw!");
         break;
     }
-
+        counter++;
 
     int[] coordinate = getCoordinate(currentPlayer, cells);
-    if (coordinate[0] == 77 )
-    {
-        Console.WriteLine($"Game played Draw!");
-        break;
-    }
-
     cells[coordinate[0], coordinate[1]] = mark;
 
     drawBoard(cells);
 }
 
 
-Console.WriteLine($"Congratulation {currentPlayer.ToUpper()}, You Wiiiiin!");
-
 static int[] getCoordinate(string currentPlayer, string[,] cells)
 {
-
     while (true)
     {
-
         try
         {
             Console.Write($"{currentPlayer.ToUpper()} enter position (x,y) or (x y): ");
-            string text = Console.ReadLine();
+            string text = Console.ReadLine() ?? "77 77";
             string[] subs = text.Trim().Split(' ', ',');
             int x = Convert.ToInt16(subs[0]);
             int y = Convert.ToInt16(subs[1]);
             if ((x < 0 || x > 2) || (y < 0 || y > 2))
-                throw new Exception("Range is 0 - 2.");
+                throw new IndexOutOfRangeException();
             if (cells[x,y] != "-")
             {
-                throw new Exception("This place is occupied.");
+                throw new TypeAccessException("This place is occupied.");
             }
             int[] coordinate = { x, y };
             return coordinate;
         }
+        catch (IndexOutOfRangeException e)
+        {
+            Console.WriteLine($"{e.Message}\b: 0, 1, 2.");
+        }
+        catch (TypeAccessException)
+        {
+            Console.WriteLine($"Cannot set position. Cell isn't empty.");
+        }
         catch
         {
-            Console.WriteLine("oeeeeeeeeee!");
+            Console.WriteLine("Please, Enter valid position!");
             continue;
         }
     }
@@ -106,9 +110,9 @@ static void drawBoard(string[,] cells)
 {
     for (int i = 0; i < 3; i++)
     {
-        Console.Write("\n");
         for (int j = 0; j < 3; j++)
             Console.Write(cells[i, j] + " ");
+        if (i != 2)
+            Console.Write("\n");
     }
-    Console.Write("\n");
 }
