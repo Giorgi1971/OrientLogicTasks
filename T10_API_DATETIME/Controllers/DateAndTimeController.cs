5057d4a1a365573bf428282429a1fc30d4f717c2
Giorgi1971 <giooorgi7@gmail.com>
2022-12-09 00:01:36 +0400

    straing api for datetime objects

---
+++ T10_API_DATETIME/Controllers/DateAndTimeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.Design;
+using T10_API_DATETIME.Models;
+
+namespace T10_API_DATETIME.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DateAndTimeController : ControllerBase
+{
+    private readonly Date_Time _dt;
+
+    public DateAndTimeController()
+    {
+        _dt = new Date_Time();
+    }
+
+    [HttpGet("currentDate")]
+    public string Current()
+    {
+        return _dt.CurrentDate.ToString("yyyy-MM-dd HH:mm");
+    }
+
+    [HttpGet("LondonDate")]
+    public string CurrentLondonDate()
+    {
+        return _dt.CurrentLondonDate.ToString("yyyy-MM-dd HH:mm");
+    }
+
+    [HttpGet("FirstDayOfPreviousMonth")]
+    public string FirstDayOfPreviousMonth()
+    {
+        return _dt.FirstDayOfPreviousMonth;
+    }
+
+    [HttpGet("LastDayOfPreviousMonth")]
+    public string LastDayOfPreviousMonth()
+    {
+        return _dt.LastDayOfPreviousMonth;
+    }
+
+    [HttpGet("Difference")]
+    public string DaysBetween(DateTime date1, DateTime date2)
+    {
+        return _dt.DaysBetween(date1, date2);
+    }
+
+    [HttpGet("DayInGeorgian")]
+    public string DayInGeorgian()
+    {
+        return _dt.DayInGeorgian();
+    }
+}
---
+++ T10_API_DATETIME/Models/Date_Time.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace T10_API_DATETIME.Models
+{
+    public class Date_Time
+    {
+        static DateTime _current = DateTime.Now;
+
+        public DateTime CurrentDate { get { return _current; } }
+        public DateTime CurrentLondonDate { get { return _current.AddHours(-4); } }
+        public string FirstDayOfPreviousMonth { get
+            {
+                var yr = DateTime.Today.Year;
+                var mn = DateTime.Today.Month;
+                var firstDay = new DateTime(yr, mn, 1);
+                return firstDay.AddMonths(-1).ToString("yyyy-MM-dd");
+            }
+        }
+        public string LastDayOfPreviousMonth { get
+            {
+                var yr = DateTime.Today.Year;
+                var mn = DateTime.Today.Month;
+                var firstDay = new DateTime(yr, mn, 1);
+                return firstDay.AddDays(-1).ToString("yyyy-MM-dd");
+            }
+        }
+
+        public bool IsLeapYear(DateTime date1)
+            {
+                var year = date1.Year;
+                return DateTime.IsLeapYear(year);
+            }
+
+        public string DaysBetween(DateTime date1, DateTime date2)
+        {
+            TimeSpan diff = date1 - date2;
+            var results = Math.Abs(diff.TotalDays);
+            return $"{results}";
+        }
+
+        public string DayInGeorgian()
+        {
+            DayOfWeek wk = DateTime.Today.DayOfWeek;
+            Console.WriteLine((byte)wk);
+            var Geodays = new Dictionary<byte, string>()
+            {
+                {1, "ორშაბათი"},{2, "სამშაბათი"},
+                {3, "ოთხშაბათი"},{4, "სამშაბათი"},
+                {5, "პარასკევი"},{6, "შაბათი"},{7, "კვირა"}
+            };
+            return Geodays[(byte)wk];
+        }
+    }
+}
+
