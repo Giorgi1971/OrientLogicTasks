a3f7be53f8bb1fade9328db29b25135fdc5117bf
Giorgi1971 <giooorgi7@gmail.com>
2022-12-14 22:04:52 +0400

    begin Football Teams read from file and save in Match class. How continue, No mind

---
+++ T11_Football/Models/Match.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace T11_Football.Models
+{
+    public class Match
+    {
+        public DateTime Date { get; set; }
+        public string? HomeTeam { get; set; }
+        public string? AwayTeam { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+        public string? Tournament { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public bool Neutral { get; set; }
+
+        public static List<Match> matches;
+
+        static Match()
+        {
+            matches = new List<Match>();
+        }
+    }
+}
---
+++ T11_Football/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using T11_Football.Models;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var ms = Match.matches;
+
+// path to the csv file
+string path = "files/football_results.csv";
+
+string[] lines = File.ReadAllLines(path);
+var i = 0;
+foreach (string line in lines)
+{
+    if (i == 0)
+    {
+        i = 1;
+        continue;
+    }
+    string[] columns = line.Split(',');
+    Match match = new Match();
+    match.Date = DateTime.Parse(columns[0]);
+    match.HomeTeam = columns[1];
+    match.AwayTeam = columns[2];
+    string myStr = columns[3];
+    Int16 a;
+    bool res = Int16.TryParse(myStr, out a);
+    if (res)
+        match.HomeScore = a;
+    else
+        match.HomeScore = -1;
+
+    myStr = columns[4];
+    //match.AwayScore = Int16.Parse(columns[4]);
+    res = Int16.TryParse(myStr, out a);
+    if (res)
+        match.AwayScore = a;
+    else
+        match.AwayScore = -1;
+    match.Tournament = columns[5];
+    match.City = columns[6];
+    match.Country = columns[7];
+    if (columns[8] == "TRUE")
+        match.Neutral = true;
+    else
+        match.Neutral = false;
+    Match.matches.Add(match);
+}
+Console.WriteLine(Match.matches.Count);
