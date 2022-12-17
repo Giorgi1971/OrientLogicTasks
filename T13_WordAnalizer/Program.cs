2345caaebcbde95843e0e1ae6d508946f9a6b771
Giorgi1971 <giooorgi7@gmail.com>
2022-12-17 17:42:05 +0400

    Task 13 Word analizer, eith multithread

---
+++ T13_WordAnalizer/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Thread mainThread = Thread.CurrentThread;
+mainThread.Name = "Main Thread";
+Console.WriteLine(mainThread.Name);
+
+
+
+
+
+
+
+Console.ReadKey();
