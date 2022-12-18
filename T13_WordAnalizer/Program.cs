dbef92b96c186f6de0d29c6cafaffa3bfe40cb02
Giorgi1971 <giooorgi7@gmail.com>
2022-12-18 17:00:42 +0400

    Finished Task 13

--- T13_WordAnalizer/Program.cs
+++ T13_WordAnalizer/Program.cs
@@ -1,17 +1,31 @@
 ﻿using T13_WordAnalizer;
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
 
-Thread mainThread = Thread.CurrentThread;
-mainThread.Name = "Main Thread";
-Console.WriteLine(mainThread.Name);
-Console.WriteLine(mainThread.Name);
-Console.WriteLine(mainThread.Name);
+var watchAll = new Stopwatch();
+//Thread mainThread = Thread.CurrentThread;
+//mainThread.Name = "Main Thread";
+//Console.WriteLine(mainThread.Name);
 
+watchAll.Start();
+
+var watch0 = Stopwatch.StartNew();
 string path = "words_alpha.txt";
-
-
 WordReader tast13 = new WordReader();
 var allText = tast13.Read(path);
+watch0.Stop();
 
+var watch1 = Stopwatch.StartNew();
 tast13.FindWords(allText);
+watch1.Stop();
+
+var watch2 = Stopwatch.StartNew();
+tast13.Dict100();
+watch2.Stop();
+
+watchAll.Stop();
+
+//tast13.DosplayAnyDict(dictVowels100);
+Console.WriteLine(watchAll.Elapsed.Duration());
+Console.WriteLine(watch0.Elapsed.Duration());
+Console.WriteLine(watch1.Elapsed.Duration());
+Console.WriteLine(watch2.Elapsed.Duration());
--- T13_WordAnalizer/WordReader.cs
+++ T13_WordAnalizer/WordReader.cs
@@ -1,21 +1,23 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace T13_WordAnalizer
 {
     public class WordReader
     {
-
-        public readonly char[] _vowels = { 'a', 'e', 'i', 'o', 'u', };
+        public readonly char[] _vowels = { 'a', 'e', 'i', 'o', 'u' };
         public string? shortWord { get; set; }
         public string? longWord { get; set; }
+        public ConcurrentDictionary<string, int>? wordVowels;
 
-        //public WordReader()
-        //{
-        //}
+        public WordReader()
+        {
+            wordVowels = new ConcurrentDictionary<string, int>();
+        }
 
         public void DoTask13()
         {
-            Console.WriteLine("Hello");
+            return;
         }
 
         public string[] Read(string path)
@@ -24,27 +26,70 @@ namespace T13_WordAnalizer
             return fileAll;
         }
 
+        public void Dict100()
+        {
+            if (wordVowels is not null)
+            {
+                var dict_100 = wordVowels
+                .OrderByDescending(d => d.Value)
+                .Take(20)
+                .ToDictionary(pair => pair.Key, pair => pair.Value);
+                //Console.WriteLine(dict_100.Count());
+                DosplayAnyDict(dict_100);
+            }
+        }
+
+        // find shor and long word. Alse create Dictionary (Word => Vowels.Count)
         public void FindWords(string[] allText)
         {
-            int shortWordLenth = int.MaxValue;
+            int shortWordLength = int.MaxValue;
 
-            int longWordLenth = 0;
+            int longWordLength = 0;
 
-            foreach (var item in allText)
+            var options = new ParallelOptions { MaxDegreeOfParallelism = 4 };
+
+
+            Parallel.ForEach(allText, options, (word) =>
+
+            //foreach (var word in allText)
             {
-                if (item.Length > longWordLenth)
+                if (word.Length > longWordLength)
                 {
-                    longWordLenth = item.Length;
-                    longWord = item;
+                    longWordLength = word.Length;
+                    longWord = word;
                 }
-                if (item.Length < shortWordLenth)
+                if (word.Length < shortWordLength)
                 {
-                    shortWordLenth = item.Length;
-                    shortWord = item;
+                    shortWordLength = word.Length;
+                    shortWord = word;
+                }
+
+                int counter = 0;
+                foreach (var letter in word)
+                {
+                    if (_vowels.Contains(letter))
+                    {
+                        counter++;
+                    }
+                }
+                if (wordVowels is not null)
+                    wordVowels.TryAdd(word, counter);
+
+            });
+            //Console.WriteLine(Dict.Count);
+            Console.WriteLine($"The shortes word's {shortWord} lenth is {shortWordLength}");
+            Console.WriteLine($"The longest word's {longWord} lenth is {longWordLength}");
+        }
+
+        public void DosplayAnyDict(Dictionary<string, int> dict)
+        {
+            if(dict != null)
+            {
+                foreach (var item in dict)
+                {
+                    System.Console.WriteLine($"Word {item.Key} has {item.Value} vowels");
                 }
             }
-            Console.WriteLine(longWord);
-            Console.WriteLine(shortWord);
         }
     }
 }
