d52ccc29280f562d456bbe19d3c73f9ff23bde76
Giorgi1971 <giooorgi7@gmail.com>
2022-12-19 01:30:49 +0400

    Task 16, looks easy

---
+++ T_16_MoveToEnd/Program.cs
@@ -0,0 +1,24 @@
+﻿var array = new List<int> { 2, 1, 2, 2, 2, 3, 4, 2 };
+var toMove = 2;
+
+MoveToEnd(array, toMove);
+Display(array);
+
+
+void MoveToEnd(List<int> array, int toMove)
+{
+    var arrayEnd = array.FindAll(n => n == toMove);
+    array.RemoveAll(n => n == toMove);
+    array.AddRange(arrayEnd);
+}
+
+void Display(List<int> array)
+{
+    foreach (var item in array)
+    {
+        Console.Write(item + ", ");
+    }
+    Console.Write("\b\b");
+}
+
+
