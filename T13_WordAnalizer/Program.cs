3033db8db93996505c1a8d91af9141a4a331c757
Giorgi1971 <giooorgi7@gmail.com>
2022-12-18 11:35:39 +0400

    Find short and long word

--- T13_WordAnalizer/Program.cs
+++ T13_WordAnalizer/Program.cs
@@ -1,14 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using T13_WordAnalizer;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 Thread mainThread = Thread.CurrentThread;
 mainThread.Name = "Main Thread";
 Console.WriteLine(mainThread.Name);
+Console.WriteLine(mainThread.Name);
+Console.WriteLine(mainThread.Name);
+
+string path = "words_alpha.txt";
 
 
+WordReader tast13 = new WordReader();
+var allText = tast13.Read(path);
 
-
-
-
-
-Console.ReadKey();
+tast13.FindWords(allText);
---
+++ T13_WordAnalizer/WordReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace T13_WordAnalizer
+{
+    public class WordReader
+    {
+
+        public readonly char[] _vowels = { 'a', 'e', 'i', 'o', 'u', };
+        public string? shortWord { get; set; }
+        public string? longWord { get; set; }
+
+        //public WordReader()
+        //{
+        //}
+
+        public void DoTask13()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public string[] Read(string path)
+        {
+            string[] fileAll = File.ReadAllLines(path);
+            return fileAll;
+        }
+
+        public void FindWords(string[] allText)
+        {
+            int shortWordLenth = int.MaxValue;
+
+            int longWordLenth = 0;
+
+            foreach (var item in allText)
+            {
+                if (item.Length > longWordLenth)
+                {
+                    longWordLenth = item.Length;
+                    longWord = item;
+                }
+                if (item.Length < shortWordLenth)
+                {
+                    shortWordLenth = item.Length;
+                    shortWord = item;
+                }
+            }
+            Console.WriteLine(longWord);
+            Console.WriteLine(shortWord);
+        }
+    }
+}
+
