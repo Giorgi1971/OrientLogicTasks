1847a3d94e49003e748aef8f5ed2e370e2569b5a
Giorgi1971 <giooorgi7@gmail.com>
2022-12-21 02:32:17 +0400

    Made Like code, uploaded by vano

---
+++ T16_MoveLikeVano/Program.cs
@@ -0,0 +1,36 @@
+﻿var array = new[] { 7, 5, 1, 8, 9, 2, 2, 3, 4, 8 };
+var toMove = 2;
+MoveElementToEnd(array, toMove);
+Console.WriteLine(string.Join(", ", array));
+
+void Swap(int[] array, int left, int right)
+{
+    (array[left], array[right]) = (array[right], array[left]);
+}
+
+void MoveElementToEnd(int[] array, int toMove)
+{
+    var right = array.Length - 1;
+
+
+    for (int i = 0; i < right; i++)
+    {
+
+        // თუ წინა წევრი არ უდრის 2-ს გააგრძელე
+        if (array[i] != toMove)
+        {
+            continue;
+        }
+        // როცა მარჯვენა მხარე უდრის 2-ს, ვნახულობთ წინა რიხვს
+        else
+        {
+            while (array[right] == toMove)
+            {
+                if (i >= right)
+                    break;
+                right--;
+            }
+        }
+        Swap(array, i, right);
+    }
+}
--- T_16_MoveToEnd/Program.cs
+++ T_16_MoveToEnd/Program.cs
@@ -1,9 +1,7 @@
-﻿var array = new List<int> { 2, 1, 2, 2, 2, 3, 4, 2 };
+﻿var array = new List<int> { 7, 5, 2, 1, 2, 2, 2, 3, 4, 2 };
 var toMove = 2;
-
 MoveToEnd(array, toMove);
-Display(array);
-
+Console.WriteLine(string.Join(", ", array));
 
 void MoveToEnd(List<int> array, int toMove)
 {
@@ -11,14 +9,3 @@ void MoveToEnd(List<int> array, int toMove)
     array.RemoveAll(n => n == toMove);
     array.AddRange(arrayEnd);
 }
-
-void Display(List<int> array)
-{
-    foreach (var item in array)
-    {
-        Console.Write(item + ", ");
-    }
-    Console.Write("\b\b");
-}
-
-
