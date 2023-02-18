c276cd267ce692036e9321ad15fd4dc5c223e87c
Giorgi1971 <giooorgi7@gmail.com>
2023-02-19 03:43:55 +0400

    try to go forward

--- P_5_RSSFeed/APIProject/Db/Entity/FeedEntity.cs
+++ P_5_RSSFeed/APIProject/Db/Entity/FeedEntity.cs
@@ -6,7 +6,7 @@ namespace APIProject.Db.Entity
         public int FeedEntityId { get; set; }
         //public object MyProperty { get; set; }
         public string Title { get; set; } = null!;
-        public string? Summary { get; set; }
+        public string? Description { get; set; }
         public string? Author { get; set; }
         public string? Photo { get; set; }
         public DateTime CreateAt { get; set; }
--- P_5_RSSFeed/ConsoleProject/Program.cs
+++ P_5_RSSFeed/ConsoleProject/Program.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Net.Http;
-using APIProject.Db.Entity;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Xml;
 using Newtonsoft.Json;
 
 public class Program
 {
+
+
     static HttpClient client = new HttpClient();
 
     static async Task Main(string[] args)
     {
-        var apiUrl = "https://localhost:7047/";
+        string feedUrl = "https://feed.infoq.com/";
+        XmlDocument rssDoc = new XmlDocument();
+        rssDoc.Load(feedUrl);
+        XmlNodeList rssItems = rssDoc.SelectNodes("rss/channel/item");
+
+        foreach (XmlNode rssItem in rssItems)
+        {
+            string title = rssItem.SelectSingleNode("title").InnerText;
+            string description = rssItem.SelectSingleNode("description").InnerText;
+            string pubDate = rssItem.SelectSingleNode("pubDate").InnerText;
+            Console.WriteLine("Title: {0}\nDescription: {1}\nPublication Date: {2}\n", title, description, pubDate);
+        }
+
+        //Console.ReadLine();
+
+        var ma1 = new MyApiModel { Name = "Gio", Age = 51 };
+        var ma2 = new MyApiModel { Name = "Nino", Age = 45 };
+
+        var apiUrl = "https://localhost:7047/api/Feed/";
         var response = await client.GetAsync(apiUrl);
         if (response.IsSuccessStatusCode)
         {
@@ -25,28 +45,28 @@ public class Program
         }
     }
 
-    static async Task Main2(string[] args)
-    {
-        var httpClient = new HttpClient();
-        httpClient.BaseAddress = new Uri("https://localhost:7047");
-        httpClient.DefaultRequestHeaders.Accept.Clear();
-        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    //static async Task Main2(string[] args)
+    //{
+    //    var httpClient = new HttpClient();
+    //    httpClient.BaseAddress = new Uri("https://localhost:7047");
+    //    httpClient.DefaultRequestHeaders.Accept.Clear();
+    //    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-        var response = await httpClient.GetAsync("api/Feed/");
-        if (response.IsSuccessStatusCode)
-        {
-            var content = await response.Content.ReadAsStringAsync();
-            var myModels = JsonConvert.DeserializeObject<List<MyApiModel>>(content);
-            foreach (var myModel in myModels)
-            {
-                Console.WriteLine($"{myModel.Name} ({myModel.Age})");
-            }
-        }
-        else
-        {
-            Console.WriteLine($"Failed to retrieve data: {response.ReasonPhrase}");
-        }
-    }
+    //    var response = await httpClient.GetAsync("api/Feed/");
+    //    if (response.IsSuccessStatusCode)
+    //    {
+    //        var content = await response.Content.ReadAsStringAsync();
+    //        var myModels = JsonConvert.DeserializeObject<List<MyApiModel>>(content);
+    //        foreach (var myModel in myModels)
+    //        {
+    //            Console.WriteLine($"{myModel.Name} ({myModel.Age})");
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine($"Failed to retrieve data: {response.ReasonPhrase}");
+    //    }
+    //}
 }
 
 public class MyApiModel
